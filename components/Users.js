a466c935ea2d0fa19688bc3eed471955283fff09
Huawei <katya.komedeva00@gmail.com>
2021-09-14 17:28:43 +0300

    react native

--- components/Users.js
+++ components/Users.js
@@ -1,15 +1,24 @@
-import React from "react";
-import {StyleSheet} from "react-native";
+import React, {useEffect, useState} from "react";
+import {FlatList, StyleSheet} from "react-native";
 import {View, Text, Button, TouchableOpacity} from "react-native";
+import {getUsers} from "../services/ServiceApi";
+import {User} from "./User";
 
-const Users =()=>{
-    return(
+const Users = () => {
+    let [users, setUsers] = useState()
+    useEffect(() => {
+        getUsers().then(value => setUsers([...value]))
+    }, [])
+    return (
         <View>
-            <Text>
-                Users page
-            </Text>
+            <FlatList
+                data={users}
+                renderItem={({item}) => <User item={item}/>}
+                keyExtractor={item => '' + item.id}
+            />
         </View>
     )
 }
 
 export {Users}
+let styles = StyleSheet.create({});
