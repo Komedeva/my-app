edfcd1e6e3b87d8e80c37e4018b2cc11850c08cf
Huawei <katya.komedeva00@gmail.com>
2021-09-14 15:44:05 +0300

    react native

---
+++ components/Users.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {StyleSheet} from "react-native";
+import {View, Text, Button, TouchableOpacity} from "react-native";
+
+const Users =()=>{
+    return(
+        <View>
+            <Text>
+                Users page
+            </Text>
+        </View>
+    )
+}
+
+export {Users}
